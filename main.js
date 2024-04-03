b2d940597a1a6e976494789693560894114b1603
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-04-03 09:35:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,4 +18,20 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-01-01T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempos = [tempoObjetivo1,tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
