3cf9fa2928fd7ec94cf265d88ca442e0d367c8eb
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-04-10 09:29:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -31,8 +31,11 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
+if(tempoFinal>0){
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}elise{
+ return "Prazo Finalizado";
+ }
 }
 function atualizaCronometro(){
     for(let i=0, i<contadores.lenght; i++){
