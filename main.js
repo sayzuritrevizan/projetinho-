1ef3435beb36dc18fae81a2edfeb13902062b5a2
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-04-29 09:28:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -31,17 +31,22 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-if(tempoFinal>0){
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-}elise{
- return "Prazo Finalizado";
- }
+if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
+    }
 }
 function atualizaCronometro(){
-    for(let i=0, i<contadores.lenght; i++){
-        contadores[i].textContent=calculaTempo(tempos[i]);
-        ]
-    ]
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+
+    for (let i=0; i<contadores.length;i++){
+       // contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
     function comecaCronometro()[
         atualizaCronometro();
         setINterval(atualizaCronometro1000);
