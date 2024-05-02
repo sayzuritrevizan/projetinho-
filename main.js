f42516b20510cf02348df434e570be5c3eebdb81
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-05-02 07:46:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,9 +16,13 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-01-01T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+let tempoAtual = new Date();
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
